41c411e9c5fb733fce0215ebbb27e349cbad9313
Muzaffar Yusupov <153423021+muzaffaryusupov1@users.noreply.github.com>
2025-05-23 20:47:00 +0500

    services section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			lightHover: '#fcf4ff',
+			darkHover: '#2a004a',
+			darkTheme: '#11001F',
+		},
+		fontFamily: {
+			Outfit: ['Outfit', 'sans-serif'],
+			Ovo: ['Ovo', 'serif'],
+		},
+		boxShadow: {
+			black: '4px 4px 0 #000',
+			white: '4px 4px 0 #fff',
+		},
+		gridTemplateColumns: {
+			auto: 'repeat(auto-fit, minmax(200px, 1fr))',
+		},
+	},
+	plugins: [],
+} satisfies Config
